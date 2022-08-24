8978ecf39ec8848f456c2f2ff27019901ed83fdd
Bartosz Borowski <bartosz.borowski@kuehne-nagel.com>
2022-08-24 13:21:50 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import abc
+
+
+class Parser(abc.ABC):
+    def __init__(self, description):
+        self.description = description
+        self.parsed_description = None
+        self.is_valid = None
+
+    @abc.abstractmethod
+    def parse(self):
+        pass
+
+
+class AwsParser(Parser):
+    pass
+
+
+class GcpParser(Parser):
+    pass
+
+
+if __name__ == '__main__':
+    """
+    aws_description spec:
+    /w{12} - any 12 alphanumerical characters 
+    "arn:aws:iam::*:" -> NOT OK
+    "arn:aws:iam::\w{12}:role/*" -> NOT OK
+    "arn:aws:iam::\w{12}:user/*" -> NOT OK
+    "arn:aws:iam::\w{12}:federated-user/*" -> NOT OK
+    "arn:aws:iam::\w{12}:oidc-provider/*" -> NOT OK
+    "arn:aws:iam::1234567890ab:role/test" -> OK
+    
+    gcp_description spec:
+    number{3} - dash number{3} - dash letter{3}
+    "123-456-abc" -> OK
+    "abc-def-123" -> NOT OK
+    "123-456*-abc" -> NOT OK
+    """
+
+    gcp_description = "user: Joe" \
+                      "email: joe.doe@example.com" \
+                      "gcp_account = 123-456-abc"
+
+    aws_description = "user: Karen" \
+                      "email: karen.doe@example.com" \
+                      "aws_arn: arn:aws:iam::1234567890ab:root"
